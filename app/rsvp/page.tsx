33736079cddb624a38df8ea7a73bd3e9e1da9816
Luke Berry <lukeberry99@gmail.com>
2024-11-05 18:23:23 +0000

    ignore eslint during builds

--- app/rsvp/page.tsx
+++ app/rsvp/page.tsx
@@ -9,8 +9,8 @@ export default function RSVP() {
         <h1 className="text-4xl font-bold text-center mb-8">RSVP</h1>
         <div className="max-w-2xl mx-auto mb-12">
           <p className="text-center mb-8">
-            We're excited to celebrate with you! Please fill out this form to
-            let us know if you'll be joining us.
+            We&apos;re excited to celebrate with you! Please fill out this form
+            to let us know if you&apos;ll be joining us.
           </p>
         </div>
         <RSVPForm />
